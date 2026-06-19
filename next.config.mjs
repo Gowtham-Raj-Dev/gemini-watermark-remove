e10d9eeba077683815d889eb371f58db46c2cad9
Gowtham-Raj-Dev <gowthamraje1221@gmail.com>
2026-06-19 17:09:05 +0530

    Add basePath for GitHub pages routing

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig = {
   output: "export",
+  basePath: isGithubActions ? "/gemini-watermark-remove" : "",
   images: {
     unoptimized: true,
   },
